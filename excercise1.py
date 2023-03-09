16426eb9a161e0525962dd97edd5658e5a22a804
Nasim09 <nasimlawrence@The-Hidden-Genius-Project-30.local>
2023-03-08 18:42:19 -0800

    nasim

---
+++ excercise1.py
@@ -0,0 +1,13 @@
+# num_1 = 20 
+# num_2 = 30/example numbers
+def multiSum(num_1, num_2): #this is a function. functions holds data types, varibles and statements
+    if num_1 * num_2 <= 1000:#1000 means it could add anything under 1000
+        return num_1 * num_2
+    
+    else:
+        return num_1 + num_2
+
+cheeseburgers = multiSum(200, 79,)
+print(cheeseburgers)#prints cheeseburger varible
+
+    
